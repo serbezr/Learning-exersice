42f441e924c3e92fb3cf76722ef384dcac411d30
Sergey Bezrukov <aviastro47@mail.ru>
2022-06-09 15:25:53 +0300

    Десятый коммит дополнение

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
 
 from .models import Question
@@ -7,14 +8,17 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context= {
-        'latest_question_list':latest_question_list,
+        'latest_question_list':latest_question_list'
             }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
