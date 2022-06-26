79b8c7937645d86d532db7b3a0f9e42101359863
Sergey Bezrukov <aviastro47@mail.ru>
2022-06-26 10:18:02 +0300

    Fourth commit #В соответствии с правилом независимости приложений: #1.Создаем файл women/urls в папке women #2.В нем прописываем: #from django.urls import path #from .views import* #urlpatterns = [   # path(' ',index) -http:127.0.01:8000/women   # path('cats/',categories) -http://127.0.0.1:8000/women/cats #3.Ввводим функцию include,чтобы отвязать women от coolsite/urls.py #coolsite/urls #from women.views import* from django.urls import path,include #urlpatterns = [ path('admin/',admin.site.urls) #path('women/',include('women.urls') #4.Запускаем лок.сервер #Вводим http://127.0.0.1:8000/women/ #http://127.0.0.1:8000/women/cats/

--- Learning-esersice2/coolsite/coolsite/urls.py
+++ Learning-esersice2/coolsite/coolsite/urls.py
@@ -13,14 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from women.views import categories
 
-from women.views import index
+
+from women.views import*
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index),
-    path('cats/',categories),
+    path('women', include('women.urls')),
 ]
