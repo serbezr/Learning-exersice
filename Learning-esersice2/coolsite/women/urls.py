2d07d33688738de54c7c4c0f8497f16729a589b1
Sergey Bezrukov <aviastro47@mail.ru>
2022-06-27 12:12:10 +0300

    women/urls.py     path(' ', index),     path('cats/<slug:cat>/',categories) ]     2.Вносим изменения     women/views.py     def caregories(request,cat)          return HttpResponce(f"<h1>Статьи по категориям</h1><p>{cat}</h1>")     3. Запускаем лок.сервер     http://127.0.0.1:8000/cats/11adf

--- Learning-esersice2/coolsite/women/urls.py
+++ Learning-esersice2/coolsite/women/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index),  # http://127.0.0.1:8000/
-    path('cats/<int:catid>/',categories), #http://127.0.0.1:8000/cats/1/
+    path('cats/<slug:cat>/',categories), #http://127.0.0.1:8000/cats/1/
 ]
--- Learning-esersice2/coolsite/women/views.py
+++ Learning-esersice2/coolsite/women/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Страница приложения women")
-def categories(reguest,catid):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+def categories(reguest,cat):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{cat}</p>")
