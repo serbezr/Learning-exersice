b249bfff20f402c81ada6d32976ca1fb385d1fe7
Sergey Bezrukov <aviastro47@mail.ru>
2022-06-29 13:56:20 +0300

    commit commit

--- Learning-esersice2/coolsite/women/views.py
+++ Learning-esersice2/coolsite/women/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Страница приложения women")
-def categories(reguest,cat):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{cat}</p>")
+    return HttpResponse("Страница приложения women.")
+
+def categories(request,catid):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
