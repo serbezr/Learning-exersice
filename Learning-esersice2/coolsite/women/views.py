48df5b8a959649cbf48b96d29b0a315bdd7d408f
Sergey Bezrukov <aviastro47@mail.ru>
2022-07-01 13:33:13 +0300

    POST-запросы

--- Learning-esersice2/coolsite/women/views.py
+++ Learning-esersice2/coolsite/women/views.py
@@ -6,8 +6,8 @@ def index(request):
     return HttpResponse("Страница приложения women.")
 
 def categories(request,catid):
-    if (request.GET):
-        print(request.GET)
+    if (request.POST):
+        print(request.POST)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
 def archive(request, year):
